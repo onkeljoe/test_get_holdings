df60d7c4341d297b40e3b879a71239890db50387
Onkeljoe <dev@onkeljoe.de>
2022-06-17 13:53:20 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Test get token holdings
+
+This script is a short test to retrieve token holdings from snapshot
+
+# start
+
+first add an enviornment variable with the address:
+
+```sh
+export MYADDRESS=###
+```
+
+replace ### with your wallet address, starting with 0x.
+
+# dependencies
+
+external module "snapshot.js" by snapshotlab. On first use add by:
+
+```sh
+npm i @snapshot-labs/snapshot.js
+```
---
+++ test.js
@@ -0,0 +1,73 @@
+const snapshot = require('@snapshot-labs/snapshot.js');
+
+// strategies reduced to the first and only relevant: staked LP on Beethoven-X
+// replaced weight and weightDecimals by the default values
+const CRE8Rstrategies = [
+    {
+      "name": "masterchef-pool-balance",
+      "network": "250",
+      "params": {
+        "pid": "39",
+        "symbol": "BEETSLP -> SLP",
+        "weight": 1, //172,
+        "tokenIndex": 0,
+        "chefAddress": "0x8166994d9ebBe5829EC86Bd81258149B87faCfd3",
+        "uniPairAddress": null,
+        "weightDecimals": 0 //3
+      }
+    },
+];
+const CRE8R = 'cre8r'
+const addresses = [process.env.MYADDRESS,] // just my address for testing
+
+//const block = 40013791 // snapshot vote round 12
+const block = 39001234 // snapshot vote round 11
+
+
+//##################################################
+
+const bribeSettings = {
+    [CRE8R]: {
+      strategies: [...CRE8Rstrategies], //cre8r strategy 2 is the erc20 balance of address
+      network: 250
+    }
+}
+  
+/**
+ * 
+ * @param {*} addresses 
+ * @param {*} blockNumber 
+ * @returns promise
+ */
+function getHoldings(addresses, blockNumber) {
+    return snapshot.utils.getScores(
+      CRE8R,
+      [...bribeSettings[CRE8R].strategies],
+      bribeSettings[CRE8R].network,
+      [...addresses],
+      blockNumber
+    ).then(scores => {
+      if (!scores) return;
+    const holdings = {}
+    for (let i = 0; i < scores.length; i++) {
+      for (let j = 0; j < Object.keys(scores[i]).length; j++) {
+        let key = Object.keys(scores[i])[j]
+        if (holdings[key] === undefined) {
+          holdings[key] = scores[i][key]
+        } else {
+          holdings[key] += scores[i][key]
+        }                             
+      }
+    }
+    for (let i = 0; i < Object.keys(holdings).length; i++) {
+      holdings[Object.keys(holdings)[i]] *= 1 //8.2 //converting vp to cre8r
+    } 
+      return holdings
+    })
+}
+
+(async () => {
+    result = await getHoldings(addresses, block)
+    console.log(result)
+})()
+  
